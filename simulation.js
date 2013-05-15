1288589476605d2d45869db94d08db0b66f12e0f
Jonah George <jmi.geor@gmail.com>
2013-05-15 00:13:58 -0700

    Nightly Update
    Added array of node objects which now spawn at random locations.
    
    Created population variable and is now displayed on node.

--- simulation.js
+++ simulation.js
@@ -1,51 +1,66 @@
-// Create the canvas
-var canvas = document.createElement("canvas");
+var canvas = document.getElementById("mainCanvas");
   // Somewhat fullscreen canvas
-  canvas.width = window.innerWidth - 25;
-  canvas.height = window.innerHeight - 25;
+  canvas.width = window.innerWidth - 20;
+  canvas.height = window.innerHeight - 20;
 
-  /* Hardcoded Canvas Size
-  canvas.width = 800;
-  canvas.height = 600;
-  */
 var ctx = canvas.getContext("2d");
 document.body.appendChild(canvas);
+  // Canvas background color
+  ctx.fillStyle="#008A2E";
+  ctx.fillRect(0,0,canvas.width, canvas.height);
 
-// Node class
+// Sidebar
+ctx.fillStyle="#140000";
+ctx.fillRect(canvas.width-150,0,canvas.width, canvas.height);
+
 function Node (x, y, size) {
   this.x = x;
   this.y = y;
   this.size = size;
-  this.color = '#00513d';
   
-  /*
-  population: null,
-  populationGrowthRate: null,
-  
-  resourceCount: null,
-  resourceProductionRate: null,
-  */
+  this.population = 5;
+  this.populationGrowthRate = null;
 
-  //alert('Node instantiated');  
+  this.resourceCount = null;
+  this.resourceProductionRate = null;
   
   this.update = function() {
-      this.size += 0.02;
+      this.population += 0.01;
   };
   
-  // Draws a circle
   this.draw = function() {
-    ctx.fillStyle = this.color;
+    // Draws a circle
+    ctx.fillStyle = "#990000";
     ctx.beginPath();
-    ctx.arc((this.x - this.size), (this.y - this.size), this.size, 0, 2*Math.PI);
+    ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI);
     ctx.stroke();
     ctx.closePath();
     ctx.fill();
+
+    // Print population count
+    ctx.fillStyle = "#FFFFFF";
+    ctx.font="14px Arial";
+    ctx.fillText(this.population.toFixed(0), this.x, this.y);
   }   
 };
 
-/*
-function Edge {};
+function Edge () {
+  this.node1 = null;
+  this.node2 = null;
+  this.distance = null;
 
+  this.draw = function() {
+    // Draw edge
+    ctx.fillStyle = "#FFFFFF";
+    ctx.beginPath();
+    ctx.moveTo(node1.x, node1.y);
+    ctx.lineTo(node2.x, node2.y);
+    ctx.stroke();
+    ctx.closePath();
+  }  
+};
+
+/*
 function Pool {};
 
 function Disease {
@@ -54,26 +69,26 @@ function Disease {
 };
 */
 
-var node1 = new Node(200,200, 50);
-var node2 = new Node(800,800, 25);
+var nodeArr = [];
+for (var i = 0; i < 10; i++) {
+    var obj = new Node(Math.random()*1000, Math.random()*1000, 30);
+    nodeArr.push(obj);
+}
 
 var reset = function () {};
 
 // Update game objects
 var update = function () {
-    node1.update();    
-    node2.update();
+  for (var i = 0; i < 10; i++) {
+    nodeArr[i].update();
+  }
 };
 
 // Draw everything
 var render = function () {
-    // Drawing edge between nodes
-    ctx.moveTo(node1.x, node1.y);
-    ctx.lineTo(node2.x, node2.y);
-    ctx.stroke();
-
-    node1.draw();
-    node2.draw();
+  for (var i = 0; i < 10; i++) {
+    nodeArr[i].draw();
+  }
 };
 
 // The main game loop
@@ -91,3 +106,4 @@ var main = function () {
 reset();
 var then = Date.now();
 setInterval(main, 1); // Execute as fast as possible 
+
